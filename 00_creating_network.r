5f27c35230c521532ef44bdd510a103147a6550f
jlinaresb <joselb1993@gmail.com>
2022-03-23 13:23:00 +0100

    network

--- 00_creating_network.r
+++ 00_creating_network.r
@@ -21,23 +21,16 @@ OPI_g = interaction_graph(interactions = interactions )
 
 # Proteins of Interest
 # ===
-POI = tibble(protein = c('STAT1', 'STAT3', 'STAT4', 'RPS6', 'MAPK3', 'MAPK1', 'MAPKAPK2', 'AKT', 'NFKB1', 'CREB', 'PTPN11'))
-# poi = mapIds(org.Hs.eg.db,
-#              keys = proteins,
-#              column = 'UNIPROT',
-#              keytype = 'ALIAS')
-
+POI = tibble(protein = c('STAT1', 'STAT3', 'STAT4', 'RPS6', 'MAPK3',
+ 'MAPK1', 'MAPKAPK2', 'AKT1', 'RELA', 'CREB1', 'PTPN11'))
+estimulations = c('TLR9', 'TLR7')
 
 # Drug targets
 # ===
 ## parse data from XML and save it to memory
-read_drugbank_xml_db("~/projects/networks/data/drugbank_all_full_database.xml/full database.xml")
-
-
-dbparser::drugs(save_csv = T, csv_path = '~/projects/networks/data/drugbank_all_full_database.xml/')
-
-drugs = dbparser::run_all_parsers()
-
+# read_drugbank_xml_db("~/projects/networks/data/drugbank_all_full_database.xml/full database.xml")
+# dbparser::drugs(save_csv = T, csv_path = '~/projects/networks/data/drugbank_all_full_database.xml/')
+# drugs = dbparser::run_all_parsers()
 
 
 # Quality control
@@ -46,7 +39,26 @@ POI = POI %>% mutate(in_OP = protein %in% interactions$target_genesymbol)
 
 # Build network
 # ===
-network = induced_subgraph(graph = OPI_g, vids = c(1:10))
+collected_path_nodes = list()
+for (i in seq_along(estimulations)) {
+  paths = shortest_paths(OPI_g, 
+                         from = estimulations[i],
+                         to = POI$protein,
+                         output = 'vpath')
+  path_nodes = lapply(paths$vpath, names) %>% unlist() %>% unique()
+  collected_path_nodes[[i]] = path_nodes
+}
+collected_path_nodes = unlist(collected_path_nodes) %>% unique()  
+nodes = c(POI$protein, estimulations, collected_path_nodes) %>% unique()
+
+network = induced_subgraph(graph = OPI_g, vids = nodes)
+
+# Annotate network
+# ===
+V(network)$node_type = ifelse(
+  V(network)$name %in% POI$protein, "POI",
+  ifelse(
+    V(network)$name %in% estimulations, "direct estimulation", "intermediate node"))
 
 # Plotting
 # ===
@@ -57,7 +69,20 @@ ggraph(
   repulserad = vcount(network)^1.2,
   coolexp = 1.1
 ) +
+  geom_edge_link(
+    aes(
+      start_cap = label_rect(node1.name),
+      end_cap = label_rect(node2.name)),
+    arrow = arrow(length = unit(4, 'mm')
+    ),
+    edge_width = .5,
+    edge_alpha = .2
+  ) +
   geom_node_point() +
+  geom_node_label(aes(label = name, color = node_type)) +
+  scale_color_discrete(
+    guide = guide_legend(title = 'Node type')
+  ) +
   theme_bw() +
   xlab("") +
   ylab("") +
--- launch.sh
+++ launch.sh
@@ -6,5 +6,5 @@
 #SBATCH -t 10:00:00
 
 
-module load cesga/2018 gcc/6.4.0 R/4.0.2
+module load cesga/2018 gcc/6.4.0 unixodbc/2.3.7 mariadb-connector-c/3.0.8 R/4.0.2
 Rscript ~/git/network-cytof/parse_drugBank.r
