cf40640acc0dea71365ff5840d682c37e47bc58a
unknown <joselb1993@gmail.com>
2022-06-29 19:23:00 +0200

    update

---
+++ 04_cytoscape/code/create_sif_files.r
@@ -0,0 +1,24 @@
+require(CellNOptR)
+
+setwd('~/git/network-cytof/')
+
+midas=CNOlist('02_preprocess_cytoff/data/GEN2.2_norm.csv')
+pknmodel=readSIF('01_create_network/data/network.sif')
+
+res = readRDS('03_run_CellNOptR/data/results.RDS')
+
+
+indices = indexFinder(midas, pknmodel, verbose = T)
+model = compressModel(pknmodel, indices)
+
+# model = preprocessing(midas, model,
+#                       expansion = T, compression = F, cutNONC = F,
+#                       verbose = T)
+
+writeScaffold(
+  modelComprExpanded = model,
+  optimResT1 = res$OptT1,
+  optimResT2 = res$OptT5,
+  modelOriginal = pknmodel, 
+  CNOlist = midas
+)
